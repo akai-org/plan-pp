9dcbdc4469826260e495d4cbad44aff09ab5e60b
happy531 <jedrzejwesolowski177@gmail.com>
2021-11-15 19:23:50 +0100

    add some routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
+import Card from "./components/UI/Card";
 
-const App = (props) => {
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/landing" element={<p>landing page</p>} />
+        <Route path="/week" element={<Card><p>week page</p></Card>} />
+        <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </BrowserRouter>
   );
