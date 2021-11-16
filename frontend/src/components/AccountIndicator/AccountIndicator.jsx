1fd0c25834adad6463f106f593792dfd71897a06
Jan Majchrzak <jan.majchrzak@protonmail.com>
2021-11-16 19:15:18 +0100

    Finish static version of the Topbar component
    Create static Clock component
    Create static AccountIndicator component
    Link Roboto font from Google Fonts
    Install prop-types library

---
+++ frontend/src/components/AccountIndicator/AccountIndicator.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as iconDown } from '../../resources/icons/icon_down.svg';
+
+const Wrapper = styled.div`
+	font-family: 'Roboto', sans-serif;
+	display: grid;
+	grid-template-columns: 1fr 25px;
+	align-items: center;
+	text-align: right;
+	column-gap: 10px;
+`;
+
+const UserName = styled.span`
+	grid-column: 1/2;
+	grid-row: 1/2;
+	font-weight: 500;
+	font-size: 1.3rem;
+`;
+
+const UserInfo = styled.span`
+	grid-column: 1/2;
+	grid-row: 2/3;
+	font-size: 0.9rem;
+`;
+
+const IconDown = styled(iconDown)`
+	grid-column: 2/3;
+	grid-row: 1/3;
+	justify-self: center;
+`;
+
+
+
+const AccountIndicator = props => {
+	return (
+		<Wrapper className={props.className}>
+			<UserName>Jan Kowalski</UserName>
+			<UserInfo>Sztuczna Inteligencja sem. I gr. 2</UserInfo>
+			<IconDown />
+		</Wrapper>
+	);
+};
+
+export default AccountIndicator;
--- frontend/src/components/Topbar/Topbar.jsx
+++ frontend/src/components/Topbar/Topbar.jsx
@@ -2,27 +2,51 @@ import React from 'react';
 import logo from '../../resources/AKAI-LOGO.png';
 import styled from 'styled-components';
 
-const Logo = styled.img`
+import Clock from '../Clock/Clock';
+import AccountIndicator from '../AccountIndicator/AccountIndicator';
+
+const LogoImg = styled.img`
 	height: 100%;
+`;
+
+const Title = styled.span`
+	font-family: 'Roboto Slab', serif;
+	font-size: 1.75em;
+	font-weight: 300;
+`;
+
+const Logo = styled.span`
+	height: 100%;
+	display: flex;
+	align-items: center;
 `;
 
 const Bar = styled.div`
 	height: 80px;
 	display: flex;
 	align-items: center;
+	justify-content: space-between;
+	position: relative;
+	padding: 0 10px;
 `;
 
-const Title = styled.span`
-	font-family: 'Roboto Slab', serif;
-	font-size: 1.75em;
+const CenteredClock = styled(Clock)`
+	position: absolute;
+	right: 50%;
+	top:14px;
+	transform: translateX(50%);
 `;
+
 
 const Topbar = props => {
 	return (
-		<Bar>
-			<Logo src={logo} />
-			<Title>AKAI Plan Zajęć</Title>
-
+		<Bar className={props.className}>
+			<Logo>
+				<LogoImg src={logo} />
+				<Title>AKAI Plan Zajęć</Title>
+			</Logo>
+			<CenteredClock />
+			<AccountIndicator />
 		</Bar>
 	);
 };
