39921bea0191683960f6e92954be8bc7522fa892
Monika <siwinskamonika@wp.pl>
2021-11-21 17:13:03 +0100

    klasy i listy pobierające dane z excela

---
+++ backend/api/utils/excel_reader_api.py
@@ -0,0 +1,212 @@
+class Teacher:
+    teacherID = ""
+    teacherFirstName = ""
+    teacherLastName = ""
+    title = ""
+
+
+teacherIDs = []
+teacherID = ""
+teacherIDs.append(teacherID)
+
+teacherFirstNames = []
+teacherFirstName = ""
+teacherFirstNames.append(teacherFirstName)
+
+teacherLastNames = []
+teacherLastName = ""
+teacherLastNames.append(teacherLastName)
+
+titles = []
+title = ""
+titles.append(title)
+
+
+class StudentGroup:
+    groupID = ""
+    groupName = ""
+
+
+groupNames = []
+groupName = ""
+groupNames.append(groupName)
+
+groupIDs = []
+groupID = ""
+groupIDs.append(groupID)
+
+# endAt i startAt jest w CoursePlan
+
+theClasses = []
+theClass = ""
+theClasses.append(theClass)
+
+Types = []
+Type = ""
+Types.append(Type)
+
+
+class CoursePlan:
+    courseID = ""
+    faculty = ""
+    course = ""
+    term = ""
+    startAt = ""
+    endAt = ""
+    firstWeekType = ""
+    studiesMode = ""
+
+
+courseIDs = []
+courseID = ""
+courseIDs.append(courseID)
+
+faculties = []
+faculty = ""
+faculties.append(faculty)
+
+courses = []
+course = ""
+courses.append(course)
+
+terms = []
+term = ""
+terms.append(term)
+
+startAts = []
+startAt = ""
+startAts.append(startAt)
+
+endAts = []
+endAt = ""
+endAts.append(endAt)
+
+firstWeekTypes = []
+firstWeekType = ""
+firstWeekTypes.append(firstWeekType)
+
+studiesModes = []
+studiesMode = ""
+studiesModes.append(studiesMode)
+
+
+class User:
+    userID = ""
+    login = ""
+    password = ""
+    group = ""
+
+
+userIDs = []
+userID = ""
+userIDs.append(userID)
+
+logins = []
+login = ""
+logins.append(login)
+
+passwords = []
+password = ""
+passwords.append(password)
+
+
+class Subject:
+    subjectID = ""
+    name = ""
+    abbr = ""
+
+
+subjectIDs = []
+subjectID = ""
+subjectIDs.append(subjectID)
+
+names = []
+name = ""
+names.append(name)
+
+abbr_s = []
+abbr = ""
+abbr_s.append(abbr)
+
+
+class Slot:
+    slotID = ""
+    dayOfWeek = ""
+    startHour = ""
+    endHour = ""
+    weekType = ""
+    description = ""
+    endAt = ""
+    startAt = ""
+
+
+slotIDs = []
+slotID = ""
+slotIDs.append(slotID)
+
+dayOfWeeks = []
+dayOfWeek = ""
+dayOfWeeks.append(dayOfWeek)
+
+startHours = []
+startHour = ""
+startHours.append(startHour)
+
+endHours = []
+endHour = ""
+endHours.append(endHour)
+
+weekTypes = []
+weekType = ""
+weekTypes.append(weekType)
+
+descriptions = []
+description = ""
+descriptions.append(description)
+
+
+class Lesson:
+    lessonID = ""
+    lessonType = ""
+    classroom = ""
+
+
+lessonTypes = []
+lessonType = ""
+lessonTypes.append(lessonType)
+
+classrooms = []
+classroom = ""
+classrooms.append(classroom)
+
+lessonIDs = []
+lessonID = ""
+lessonIDs.append(lessonID)
+
+
+class Modification:
+    modificationType = ""
+    modificationID = ""
+
+
+modificationTypes = []
+modificationType = ""
+modificationTypes.append(modificationType)
+
+modificationIDs = []
+modificationID = ""
+modificationIDs.append(modificationID)
+
+
+class OtherActivity:
+    otherActivityTitle = ""
+    otherActivityID = ""
+
+
+OtherActivityTitles = []
+OtherActivityTitle = ""
+OtherActivityTitles.append(OtherActivityTitle)
+
+OtherActivityIDs = []
+OtherActivityID = ""
+OtherActivityIDs.append(OtherActivityID)
