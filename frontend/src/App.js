8f44781aa3666ee530d7e5c236443709578eab44
Szymon Siemieniuk <szymon.siemieniuk@student.put.poznan.pl>
2022-05-18 10:50:23 +0200

    Hello world (base: https://tinyurl.com/42k6urau)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,68 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+// import axios from 'axios';
+import { Button, Container, Card, Row } from 'react-bootstrap';
 
-function App() {
-  const [data, setData] = React.useState(null);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      setBookName: '',
+      setReview: '',
+      fetchData: [],
+      reviewUpdate: ''
+    }
+  }
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  handleChange = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value
+    this.setState({
+      [nam]: val
+    })
+  }
+  
+  handleChange2 = (event) => {
+    this.setState({
+      reviewUpdate: event.target.value
+    })
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+  render() {
+    let card = this.state.fetchData.map((val, key) => {
+        return (
+            <React.Fragment>
+                <Card style={{ width: '18rem' }} className='m-2'>
+                    <Card.Body>
+                        <Card.Title>{val.book_name}</Card.Title>
+                        <Card.Text>
+                            {val.book_review}
+                        </Card.Text>
+                        <input name='reviewUpdate' onChange={this.handleChange2} placeholder='Update Review' ></input>
+                        <Button className='m-2' onClick={() => { this.edit(val.id) }}>Update</Button>
+                        <Button onClick={() => { this.delete(val.id) }}>Delete</Button>
+                    </Card.Body>
+                </Card>
+            </React.Fragment>
+        )
+    })
+  
+    return (
+        <div className='App'>
+            <h1>Dockerized Fullstack React Application</h1>
+            <div className='form'>
+                <input name='setBookName' placeholder='Enter Book Name' onChange={this.handleChange} />
+                <input name='setReview' placeholder='Enter Review' onChange={this.handleChange} />
+            </div>
+            <Button className='my-2' variant="primary" onClick={this.submit}>Submit</Button> <br /><br />
+            <Container>
+                <Row>
+                    {card}
+                </Row>
+            </Container>
+            <h2>FREEO</h2>
+        </div>
+    );
+  }
 }
-
 export default App;
