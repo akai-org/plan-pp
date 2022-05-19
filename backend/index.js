75a9a810b45bd2624348f90081249b4f1c59138b
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2022-05-19 02:28:36 +0200

    fixed Hello World

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,8 @@ app.get('/', (req, res) => {
 
 //get all of the books in the database
 app.get('/get', (req, res) => {
-  const SelectQuery = " SELECT * FROM books_reviews";
-  db.query(SelectQuery, (err, result) => {
+  const SelectQuery = "SELECT * FROM books_reviews";
+  client.query(SelectQuery, (err, result) => {
     res.send(result)
   })
 })
@@ -41,17 +41,18 @@ app.get('/get', (req, res) => {
 app.post("/insert", (req, res) => {
   const bookName = req.body.setBookName;
   const bookReview = req.body.setReview;
-  const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
-  db.query(InsertQuery, [bookName, bookReview], (err, result) => {
+  const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES ($1, $2)";
+  client.query(InsertQuery, [bookName, bookReview], (err, result) => {
     console.log(result)
   })
 })
 
 // delete a book from the database
 app.delete("/delete/:bookId", (req, res) => {
+  console.log("DELETE")
   const bookId = req.params.bookId;
-  const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
-  db.query(DeleteQuery, bookId, (err, result) => {
+  const DeleteQuery = "DELETE FROM books_reviews WHERE id = $1";
+  client.query(DeleteQuery, [bookId], (err, result) => {
     if (err) console.log(err);
   })
 })
@@ -60,8 +61,8 @@ app.delete("/delete/:bookId", (req, res) => {
 app.put("/update/:bookId", (req, res) => {
   const bookReview = req.body.reviewUpdate;
   const bookId = req.params.bookId;
-  const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
-  db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
+  const UpdateQuery = "UPDATE books_reviews SET book_review = $1 WHERE id = $2";
+  client.query(UpdateQuery, [bookReview, bookId], (err, result) => {
     if (err) console.log(err)
   })
 })
--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,6 @@
 -- This query is just for the test. Nothing meaningful for the project.
-CREATE TABLE IF NOT EXISTS `books_reviews` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `book_name` varchar(50) NOT NULL,
-  `book_review` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS books_reviews (
+  id SERIAL PRIMARY KEY,
+  book_name varchar(50) NOT NULL,
+  book_review varchar(50) NOT NULL
+)
